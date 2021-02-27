68ec1c54df6145f990cbe8e537e389c3a568c3c2
wagnerDestro <wagner.leandro28@outlook.com>
2021-02-27 14:13:58 -0300

    Adição de funcionalidades
    O app foi estilizado, utilizando uma background image no título e foi adicionado um ícone para remover as tarefas. A remoção está funcionando, mas falta permitir a adição de novas tarefas e a persistência dos dados mesmo caso o aplicativo seja encerrado pelo usuário.

--- App.js
+++ App.js
@@ -1,21 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import AppLoading from 'expo-app-loading';
+import {AntDesign} from '@expo/vector-icons';
+import {useFonts, Lato_400Regular} from '@expo-google-fonts/lato'
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
 
 export default function App() {
+
+  const image = require('./resources/bg.jpg');
+
+  console.disableYellowBox = true;
+
+  const [tarefas, setarTarefas] = useState([
+  {
+    id: 1,
+    tarefa: 'Minha tarefa 1.'
+  },
+  {
+    id: 2,
+    tarefa: 'Minha outra tarefa.'
+  }
+]);
+
+  let [fontsLoaded] = useFonts({
+    Lato_400Regular,
+  });
+
+  if (!fontsLoaded){
+    return <AppLoading />;
+  }
+
+  function deletarTarefa(id){
+    alert('tarefa com id ' + id + ' foi deletada');
+    let newTarefas = tarefas.filter(function(val){
+      return val.id != id;
+    });
+
+    setarTarefas(newTarefas);
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={{flex:1}}>
+    <StatusBar hidden />
+      <ImageBackground source={image} style={styles.image}>
+      <View style={styles.coverView}>
+        <Text style={styles.textHeader}>Lista de Tarefas</Text>
+      </View>
+      </ImageBackground>
+
+      {
+      
+      tarefas.map(function(val){
+        return(<View style={styles.tarefaSingle}>
+          <View style={{flex:1, width:'100%', padding:5}}>
+            <Text>{val.tarefa}</Text>
+          </View>
+          <View style={{alignItems:'flex-end', flex:1, padding:10}}>
+            <TouchableOpacity onPress={() => deletarTarefa(val.id)} ><AntDesign name="minuscircleo" size={24} color="black"></AntDesign></TouchableOpacity>
+          </View>
+        </View>
+        );
+      })
+      
+      
+      }
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image: {
+    resizeMode: "cover",
+    width:'100%',
+    height:50
   },
+
+  coverView:{
+    width:'100%',
+    height:50,
+    backgroundColor:'rgba(0, 0, 0, 0.5)'
+  },
+
+  textHeader:{
+    textAlign:'center',
+    color:'white',
+    fontSize:23,
+    marginTop:20,
+    fontFamily:'Lato_400Regular'
+  },
+
+  tarefaSingle:{
+    marginTop:30,
+    width:'100%',
+    borderBottomWidth:1,
+    borderBottomColor:"black",
+    flexDirection:'row',
+    paddingBottom:10
+  }
 });
